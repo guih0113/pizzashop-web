554fd740acbcd0ccaff8a2ee5489a14fcc724683
guih0113 <gui.henriquess13@gmail.com>
2025-08-09 17:55:17 -0300

    connect to profile routes, with asn optimistic interface

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,5 +1,9 @@
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
+import { getManageRestaurant } from '@/api/get-managed-restaurant'
+import { getProfile } from '@/api/get-profile'
 import { Button } from './ui/button'
+import { Dialog, DialogTrigger } from './ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,31 +12,60 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
+import { StoreProfileDialog } from './store-profile-dialog'
 
 export function AccountMenu() {
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+    staleTime: Infinity
+  })
+
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManageRestaurant,
+    staleTime: Infinity
+  })
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="flex select-none items-center gap-2">
-          Pizza Shop
-          <ChevronDown />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          <span>Guilherme Henrique</span>
-          <span className="font-normal text-muted-foreground text-xs">gui.henriquess13@gmail.com</span>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Building className="size-4" />
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <LogOut className="size-4 text-rose-500 dark:text-rose-400" />
-          <span className='text-rose-500 dark:text-rose-400'>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex select-none items-center gap-2">
+            {isLoadingManagedRestaurant ? <Skeleton className="h-4 w-40" /> : managedRestaurant?.name}
+            <ChevronDown />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="font-normal text-muted-foreground text-xs">{profile?.email}</span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="size-4" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuItem>
+            <LogOut className="size-4 text-rose-500 dark:text-rose-400" />
+            <span className="text-rose-500 dark:text-rose-400">Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog />
+    </Dialog>
   )
 }

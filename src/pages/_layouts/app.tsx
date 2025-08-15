a30aee0de610b7a7d221dd18b012fd43958fff02
guih0113 <gui.henriquess13@gmail.com>
2025-08-15 01:07:15 -0300

    organize imports

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,24 +1,24 @@
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { Header } from '@/components/header'
-import { useEffect } from 'react'
 import { api } from '@/lib/axios'
-import { isAxiosError } from 'axios'
 
 export function AppLayout() {
   const navigate = useNavigate()
 
   useEffect(() => {
     const interceptorId = api.interceptors.response.use(
-      response => response,
-      error => {
-        if(isAxiosError(error)) {
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
           const status = error.response?.status
           const code = error.response?.data.code
 
-          if(status === 401 && code === 'UNAUTHORIZED') {
+          if (status === 401 && code === 'UNAUTHORIZED') {
             navigate('/sign-in', { replace: true })
           }
-        } 
+        }
       }
     )
 

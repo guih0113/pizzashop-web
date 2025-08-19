22aa743c52998d0c0d0689159e56a297484d89fb
guih0113 <gui.henriquess13@gmail.com>
2025-08-19 15:42:27 -0300

    configure the test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
+import type { InlineConfig } from 'vitest/node'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
-    } 
+    }
+  },
+  test: {
+    globals: true
   }
+} as UserConfig & {
+  test: InlineConfig
 })

37f9a5e8cdb25efad6fb74292ec863c6f08fbda4
guih0113 <gui.henriquess13@gmail.com>
2025-08-24 18:06:58 -0300

    msw setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { enableMSW } from './api/mocks/index.ts'
 import App from './app.tsx'
 
-// biome-ignore lint/style/noNonNullAssertion: <>
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+enableMSW().then(() => {
+  // biome-ignore lint/style/noNonNullAssertion: <>
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+})

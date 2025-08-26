400334202786ad99ed523b253f79438418d49310
guih0113 <gui.henriquess13@gmail.com>
2025-08-26 16:25:26 -0300

    create orders mocks

---
+++ src/api/mocks/get-orders-details-mock.ts
@@ -0,0 +1,33 @@
+import { HttpResponse, http } from 'msw'
+import type { GetOrderDetailsParams, GetOrderDetailsResponse } from '../get-order-details'
+
+export const getOrderDetailsMock = http.get<GetOrderDetailsParams, never, GetOrderDetailsResponse>(
+  '/orders/:orderId',
+  ({ params }) => {
+    return HttpResponse.json({
+      id: params.orderId,
+      customer: {
+        name: 'Test Name',
+        email: 'teste@example.com',
+        phone: '233423435',
+      },
+      status: 'pending',
+      createdAt: new Date().toISOString(),
+      totalInCents: 5000,
+      orderItems: [
+        {
+          id: 'order-item-1',
+          priceInCents: 1000,
+          product: { name: 'Pizza Teste' },
+          quantity: 1
+        },
+        {
+          id: 'order-item-2',
+          priceInCents: 2000,
+          product: { name: 'Pizza de Teste' },
+          quantity: 2
+        }
+      ]
+    })
+  }
+)

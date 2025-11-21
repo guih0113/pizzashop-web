b0845d95116c954f47e49438df9d44fa8808b28d
guih0113 <gui.henriquess13@gmail.com>
2025-11-21 15:36:05 -0300

    fix import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { enableMSW } from './api/mocks'
-import App from './app'
+import App from './app.tsx'
 
 enableMSW().then(() => {
   // biome-ignore lint/style/noNonNullAssertion: <>
